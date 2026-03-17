bd73cd102bc71893d6cd713e922d8ddb17d6e5d9
zwirok-git <oleksijprokopenko@gmail.com>
2026-03-17 19:00:19 +0200

    implement 3 functions: format_linter_error, format_single_linter_file,format_linter_report

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {"line": error["line_number"],
+            "column": error["column_number"],
+            "message": error["text"],
+            "name": error["code"],
+            "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {"errors": [format_linter_error(error)for error in errors],
+            "path": file_path,
+            "status": "failed" if errors else "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [format_single_linter_file(file_path, errors_file)
+            for file_path, errors_file in linter_report.items()]
