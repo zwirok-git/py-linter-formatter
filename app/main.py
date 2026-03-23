384bd25aa6efcea2dde489eca98c37fef843a2e3
Oleksii Prokopenko <oleksijprokopenko@gmail.com>
2026-03-23 09:54:34 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 def format_linter_error(error: dict) -> dict:
-    return {
-            "line": error["line_number"],
+    return {"line": error["line_number"],
             "column": error["column_number"],
             "message": error["text"],
             "name": error["code"],
-            "source": "flake8"
-    }
+            "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
@@ -15,7 +13,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [
-            format_single_linter_file(file_path, errors_file)
-            for file_path, errors_file in linter_report.items()
-    ]
+    return [format_single_linter_file(file_path, errors_file)
+            for file_path, errors_file in linter_report.items()]
